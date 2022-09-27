09b82a1b7be24d12d41ee829ba0b79803e13a11c
ValeriyMakushev <makushevva@mail.ru>
2022-09-27 23:29:51 +0400

    Task19Way_2

---
+++ Task19Way_2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 19 второй способ.
+
+//Console.WriteLine("Введите пятизначное число: ");
+//int num = Console.ReadLine();
+
+
+void Task19()
+{
+        int num = CheckNumber();
+        string chars = num.ToString();
+        if (chars[0] == chars[4] && chars[1] == chars[3])
+        {
+                Console.Write("Число является палиндромом");
+        }
+        else
+        {
+                Console.Write("Число не является палиндромом");
+        }
+}
+
+int CheckNumber()
+{
+        while (true)
+        {
+                Console.Write("Ведите пятизначное число: ");
+                int number = Convert.ToInt32(Console.ReadLine());
+                if (number > 9999 && number < 100000)
+                        return number;
+        }
+}
+Task19();
+
+
+
+//Console.WriteLine("Hello, World!");
