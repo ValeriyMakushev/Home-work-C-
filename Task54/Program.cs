ab7b3eee69a3f0d3cdd585aca219a5778f005ac7
ValeriyMakushev <makushevva@mail.ru>
2022-10-13 20:53:18 +0400

    Task54

---
+++ Task54/Program.cs
@@ -0,0 +1,73 @@
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию 
+элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int colons = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[rows, colons];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(0, 100);
+    }
+}
+
+
+void PrintArray(int[,] massiv)
+{
+    for (int i = 0; i < massiv.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < massiv.GetLength(1); j++)
+        {
+            Console.Write("  " + massiv[i, j] + " ");
+        }
+        Console.WriteLine("]");
+    }
+}
+PrintArray(array);
+
+void SortDescendingNumber(int[,] numbers)
+{
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            for (int k = 0; k < numbers.GetLength(1) - 1; k++)
+            {
+                if (numbers[i, k] < numbers[i, k + 1])
+                {
+                    int temp = numbers[i, k + 1];
+                    numbers[i, k + 1] = numbers[i, k];
+                    numbers[i, k] = temp;
+                }
+            }
+        }
+
+    }
+
+}
+/*void PrintArray(int[,] numbers)
+{
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for(int j=0; j<numbers.GetLength(1); j++)
+        Console.Write($"{numbers[i,j]}");
+    }
+    Console.WriteLine();
+} */
+SortDescendingNumber(array);
+Console.WriteLine("___________________");
+PrintArray(array);
