b70fb02a1bcde50bcd6c37d2456862cc249c2173
ValeriyMakushev <makushevva@mail.ru>
2022-10-17 14:32:16 +0400

    Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов 
+в промежутке от M до N с помощью рекурсии.
+M = 1; N = 15 -> 120
+M = 4; N = 8 -> 30*/
+
+
+
+ 
+ int PrintSumNumber(int M, int N)
+ {
+    if( N > M )
+    return M + PrintSumNumber(M+1,N);
+    else return M;
+
+
+  
+ }
+int sum =  PrintSumNumber(1,15);
+Console.WriteLine(sum);
